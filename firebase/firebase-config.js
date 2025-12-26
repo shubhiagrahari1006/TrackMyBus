68f651553a84cd32db91bbaee9498c74659afe7a
Shubhi Agarhari <shubhigrhr@gmail.com>
2025-12-26 21:21:28 +0530

    Add files via upload

---
+++ firebase/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAIQCIjm95fD6zRXg7jwatjrgV3TzIebXI",
+  authDomain: "trackmybus-b373a.firebaseapp.com",
+  databaseURL: "https://trackmybus-b373a-default-rtdb.firebaseio.com",
+  projectId: "trackmybus-b373a",
+  storageBucket: "trackmybus-b373a.appspot.com",
+  messagingSenderId: "78573520148",
+  appId: "1:78573520148:web:50bddf2b6d737664c02e01"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
