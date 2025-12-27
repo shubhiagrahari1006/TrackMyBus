efbb03cbc93adc6d5016fa4ca3121c3d82fb5f5b
Shubhi Agrahari <shubhigrhr@gmail.com>
2025-12-27 17:21:42 +0530

    Fix driver.js merge conflict

--- driver.js
+++ driver.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-function updateStatus() {
-  const route = document.getElementById("route").value;
-  const stop = document.getElementById("stop").value;
-  const time = document.getElementById("time").value;
-  const seats = document.getElementById("seats").value;
-
-  database.ref("bus/bus1").update({
-    route: route,
-    currentStop: stop,   // 👈 ab koi bhi stop kaam karega
-    arrivalTime: time,
-    seats: seats,
-    status: "Arrived",
-    lastUpdated: Date.now()
-  });
-
-  document.getElementById("status").innerText =
-    "Updated successfully!";
-}
-=======
 function updateStatus() {
   const route = document.getElementById("route").value;
   const stop = document.getElementById("stop").value;
@@ -34,6 +14,5 @@ function updateStatus() {
   });
 
   document.getElementById("status").innerText =
-    "✔ Data updated successfully";
+    "✔ Data updated successfully!";
 }
->>>>>>> 1b14f220eae89373b3086d4fb1ccb908998987cc
